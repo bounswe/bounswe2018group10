76cdc76792b52f59b36cc48ff17c07f2a6ce28ed
Abdussamet Kocak <sssmmt@gmail.com>
2018-04-13 20:59:12 +0300

    prefer environment variables instead of secrets file

--- twapi.py
+++ twapi.py
@@ -1,7 +1,18 @@
 import tweepy
 import yaml
+import os
 
-_secrets = yaml.load(open('.secrets.yaml', 'r'))
+if 'TWITTER_API_KEY' in os.environ:
+    _secrets = {
+        'api_key': os.environ['TWITTER_API_KEY'],
+        'api_secret': os.environ['TWITTER_API_SECRET'],
+        'access_token': os.environ['TWITTER_ACCESS_TOKEN'],
+        'access_token_secret': os.environ['TWITTER_ACCESS_SECRET']
+    }
+elif os.path.isfile('.secrets.yaml'):
+    _secrets = yaml.load(open('.secrets.yaml', 'r'))
+else:
+    raise SystemError('No API credentials given')
 
 
 def get_api():
@@ -11,3 +22,4 @@ def get_api():
 
 
 api = get_api()
+cursor = tweepy.Cursor
