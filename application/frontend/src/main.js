f04053eceb0f06a52326c7e4e018a65a9a901369
Baran Kılıç <baran.kilic@boun.edu.tr>
2018-10-13 17:04:00 +0300

    Add Vue Router plugin

--- application/frontend/src/main.js
+++ application/frontend/src/main.js
@@ -2,9 +2,11 @@ import '@babel/polyfill'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ application/frontend/src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = []
+
+// eslint-disable-next-line no-new
+const router = new VueRouter({
+  routes
+})
+
+export default router
