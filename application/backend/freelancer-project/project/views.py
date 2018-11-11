2e879574d5b45bc5732a74749b00f5b4eb6d4d46
Ahmet Faruk Çelimli <afcelimli@gmail.com>
2018-11-11 14:57:33 +0200

    Second update for search fields

--- application/backend/freelancer-project/project/views.py
+++ application/backend/freelancer-project/project/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import filters
-from rest_framework.response import Response
+
 
 
 from . import serializers
@@ -63,8 +63,6 @@ class BidViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user_id=self.request.user)
-
-
 
 
 
