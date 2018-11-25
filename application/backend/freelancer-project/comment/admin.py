10a7d6a8c57b707e5369f70f0fcc57023f5755d0
Ahmet Faruk Çelimli <afcelimli@gmail.com>
2018-11-25 12:39:56 +0200

    Models added to admin panel

--- application/backend/freelancer-project/comment/admin.py
+++ application/backend/freelancer-project/comment/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.FreelancerComment)    
+admin.site.register(models.ClientComment)
+
