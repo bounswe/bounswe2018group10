1ac348a57f9a097df24ff3841f124986df640488
Ahmet Faruk Çelimli <afcelimli@gmail.com>
2018-11-25 12:44:38 +0200

    Views for file comment app written

--- application/backend/freelancer-project/comment/admin.py
+++ application/backend/freelancer-project/comment/admin.py
@@ -3,6 +3,6 @@ from . import models
 
 # Register your models here.
 
-admin.site.register(models.FreelancerComment)    
+admin.site.register(models.FreelancerComment)
 admin.site.register(models.ClientComment)
 
--- application/backend/freelancer-project/comment/views.py
+++ application/backend/freelancer-project/comment/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import filters
+
+
+from . import serializers
+from . import models
+from . import permissions
 
 # Create your views here.
+
+
+class FreelancerCommentViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.FreelancerCommentSerializer
+    queryset = models.FreelancerComment.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=profile_id__id',)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateComment, IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user)
+
+
+class ClientCommentViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ClientCommentSerializer
+    queryset = models.ClientComment.objects.all()
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=profile_id__id',)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateComment, IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user)
+
+
