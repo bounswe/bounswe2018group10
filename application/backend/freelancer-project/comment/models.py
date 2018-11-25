90edb4abfa5136df157f4b3a49f7aaeb2f546f50
Ahmet Faruk Çelimli <afcelimli@gmail.com>
2018-11-25 12:32:15 +0200

    Comment models added

--- application/backend/freelancer-project/comment/models.py
+++ application/backend/freelancer-project/comment/models.py
@@ -1,3 +1,29 @@
+
 from django.db import models
 
 # Create your models here.
+
+
+
+class FreelancerComment(models.Model):
+
+    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    freelancer_profile_id = models.ForeignKey('user.FreelancerProfile', on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class ClientComment(models.Model):
+
+    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    freelancer_profile_id = models.ForeignKey('user.ClientProfile', on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
