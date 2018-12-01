445d41a4d7fec4cb5756999b2ea0ad3c6e8d16e1
Ahmet Faruk Çelimli <afcelimli@gmail.com>
2018-12-01 21:31:28 +0200

    Wallet and Payment models added

--- application/backend/freelancer-project/payment/models.py
+++ application/backend/freelancer-project/payment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Wallet(models.Model):
+    user_id = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    budget = models.IntegerField(default=0, blank=True)
+
+
+class Payment(models.Model):
+    acceptedproject_id = models.ForeignKey('acceptedProject.AcceptedProject', on_delete=models.CASCADE)
+    amount = models.IntegerField()
