735831bb0d7968a1338a03cf55b3ccfcd3ee16c1
Ahmet Faruk Çelimli <afcelimli@gmail.com>
2018-12-01 22:23:26 +0200

    Money transaction implemented

---
+++ application/backend/freelancer-project/payment/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class WalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Wallet
+        fields = ('id', 'user_id', 'budget')
+
+        extra_kwargs = {'user_id': {'read_only': True}}
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Payment
+        fields = ('id', 'acceptedproject_id', 'amount')
+
--- application/backend/freelancer-project/payment/views.py
+++ application/backend/freelancer-project/payment/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import filters
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+
+from acceptedProject.models import AcceptedProject
+from user.models import User
+from . import models
+from . import serializers
 
 # Create your views here.
+
+
+class WalletViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.WalletSerializer
+    queryset = models.Wallet.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticatedOrReadOnly)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=user_id__id')
+
+    def perform_create(self, serializer):
+        serializer.save(user_id=self.request.user)
+
+
+
+
+@api_view(['GET', 'POST'])
+def payment(request):
+    if request.method != 'POST':
+        return Response('Only POST method is allowed!', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    saveable = serializers.PaymentSerializer(data=request.data)
+    if not saveable.is_valid():
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    acceptedproject_id = saveable.validated_data['acceptedproject_id']
+    amount = saveable.validated_data['amount']
+
+    chosen_project = AcceptedProject.objects.filter(id=acceptedproject_id)
+    #client = User.objects.get(id=chosen_project.values_list('user_id', flat=True)[0])
+    #freelancer = User.objects.get(id=chosen_project.values_list('freelancer_id', flat=True)[0])
+    client_id = chosen_project.values_list('user_id', flat=True)[0]
+    freelancer_id = chosen_project.values_list('freelancer_id', flat=True)[0]
+
+    client_wallet = models.Wallet.objects.get(user_id=client_id)
+    client_wallet.budget = client_wallet.budget - amount
+    client_wallet.save()
+
+    freelancer_wallet = models.Wallet.objects.get(user_id=freelancer_id)
+    freelancer_wallet.budget = freelancer_wallet.budget - amount
+    freelancer_wallet.save()
