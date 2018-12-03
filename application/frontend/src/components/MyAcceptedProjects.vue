3b6618e81ff3951f28b5a96f3f177b3ac4f0d6af
Baran Kılıç <baran.kilic@boun.edu.tr>
2018-12-04 00:15:53 +0300

    Add some descriptions to pages

--- application/frontend/src/components/MyAcceptedProjects.vue
+++ application/frontend/src/components/MyAcceptedProjects.vue
@@ -6,49 +6,64 @@
       <b-row>
         <b-col>
           <h2>My Accepted Projects</h2>
+          <h6 v-if="isClient" class="text-muted">Projects that I created and choose a freelancer for it</h6>
+          <h6 v-if="isFreelancer" class="text-muted">Projects that I bid for and choosen as a freelancer</h6>
         </b-col>
       </b-row>
 
       <b-row>
         <b-col>
-          <b-list-group>
+          <b-list-group v-if="isClient">
             <b-list-group-item :key="project.id" v-for="project in projects">
-              <router-link :to="`/accepted-project/${project.id}`">
-                {{project.title}}
-              </router-link>
+              <router-link :to="`/accepted-project/${project.id}`">{{project.title}}</router-link>
+              <div>{{project.description | shortDescription}}</div>
+            </b-list-group-item>
+          </b-list-group>
+          <b-list-group v-if="isFreelancer">
+            <b-list-group-item :key="project.id" v-for="project in freelancerProjects">
+              <router-link :to="`/accepted-project/${project.id}`">{{project.title}}</router-link>
               <div>{{project.description | shortDescription}}</div>
             </b-list-group-item>
           </b-list-group>
         </b-col>
       </b-row>
-
     </b-container>
-      
   </div>
 </template>
 
 <script>
-import NavigationBar from './NavigationBar.vue'
+import NavigationBar from "./NavigationBar.vue";
 
 export default {
   name: "MyAcceptedProjects",
   components: {
     NavigationBar
   },
-  data () {
+  data() {
     return {
       projects: [],
-    }
+      freelancerProjects: []
+    };
   },
   created() {
     this.fetchData();
   },
   methods: {
-    fetchData(){
+    fetchData() {
       this.$axios
         .get(`/acceptedproject/create/?search=${this.$root.$data.user_id}`)
         .then(response => {
           this.projects = response.data;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      this.$axios
+        .get(`/acceptedproject/create/`)
+        .then(response => {
+          this.freelancerProjects = response.data.filter(
+            proj => proj.freelancer_id == this.$root.$data.user_id
+          );
         })
         .catch(err => {
           console.log(err);
--- application/frontend/src/components/MyProjects.vue
+++ application/frontend/src/components/MyProjects.vue
@@ -6,49 +6,74 @@
       <b-row>
         <b-col>
           <h2>My Projects</h2>
+          <h6 v-if="isClient" class="text-muted">Projects that I created</h6>
+          <h6 v-if="isFreelancer" class="text-muted">Projects that I bid for</h6>
         </b-col>
       </b-row>
 
       <b-row>
         <b-col>
-          <b-list-group>
+          <b-list-group v-if="isClient">
             <b-list-group-item :key="project.id" v-for="project in projects">
-              <router-link :to="`/project/${project.id}`">
-                {{project.title}}
-              </router-link>
+              <router-link :to="`/project/${project.id}`">{{project.title}}</router-link>
+              <div>{{project.description | shortDescription}}</div>
+            </b-list-group-item>
+          </b-list-group>
+          <b-list-group v-if="isFreelancer">
+            <b-list-group-item :key="project.id" v-for="project in freelancerProjects">
+              <router-link :to="`/project/${project.id}`">{{project.title}}</router-link>
               <div>{{project.description | shortDescription}}</div>
             </b-list-group-item>
           </b-list-group>
         </b-col>
       </b-row>
-
     </b-container>
-      
   </div>
 </template>
 
 <script>
-import NavigationBar from './NavigationBar.vue'
+import NavigationBar from "./NavigationBar.vue";
 
 export default {
   name: "MyProjects",
   components: {
     NavigationBar
   },
-  data () {
+  data() {
     return {
       projects: [],
-    }
+      freelancerProjects: []
+    };
   },
   created() {
     this.fetchData();
   },
   methods: {
-    fetchData(){
+    fetchData() {
       this.$axios
         .get(`/project/create/?search=${this.$root.$data.user_id}`)
         .then(response => {
           this.projects = response.data;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      this.$axios
+        .get(`/project/bid/`)
+        .then(response => {
+          let projectList = response.data.filter(
+            bid => bid.user_id == this.$root.$data.user_id
+          ).map(bidList => bidList.project_id);
+          this.$axios
+            .get(`/project/create/`)
+            .then(response => {
+              this.freelancerProjects = response.data.filter(proj =>
+                projectList.includes(proj.id)
+              );
+            })
+            .catch(err => {
+              console.log(err);
+            });
         })
         .catch(err => {
           console.log(err);
