73187280521b1ede946da585b693fdcd7b129cf5
Ahmet Faruk Çelimli <afcelimli@gmail.com>
2018-12-08 01:41:59 +0200

    Filtering and ordering for project

--- application/backend/freelancer-project/freelancer/settings.py
+++ application/backend/freelancer-project/freelancer/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'comment.apps.CommentConfig',
     'acceptedProject.apps.AcceptedprojectConfig',
     'payment.apps.PaymentConfig',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- application/backend/freelancer-project/project/views.py
+++ application/backend/freelancer-project/project/views.py
@@ -4,7 +4,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import filters
 
-
+from django_filters import rest_framework as filter
 
 from . import serializers
 from . import models
@@ -20,9 +20,9 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = models.Project.objects.all()             # tells the viewset how to retrieve data from database
     authentication_classes = (TokenAuthentication,)     # defines which authentication type will be used
     permission_classes = (permissions.UpdateProject, IsAuthenticatedOrReadOnly)   # defines users' permissions
-    filter_backends = (filters.SearchFilter,)           # defines which filter type will be used
+    filter_backends = (filters.SearchFilter, filter.DjangoFilterBackend, filters.OrderingFilter)          # defines which filter type will be used
     search_fields = ('title', 'tags__title', 'category__title', '=user_id__id')  # to filter with a foreign key, field
-                                                                                  # name in the related table must be
+    filter_fields = ('deadline', 'budget_min', 'budget_max')                    # name in the related table must be
                                                                                   # written after name of the field
 
 
