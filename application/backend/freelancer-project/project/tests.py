939177eed0c98a60996ce1eda9ec9718a135c6ad
Ahmet Faruk Çelimli <afcelimli@gmail.com>
2019-01-06 23:45:28 +0200

    Unit test added for Tag model

--- application/backend/freelancer-project/project/tests.py
+++ application/backend/freelancer-project/project/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Tag
+
 
 # Create your tests here.
+# models test
+class TagTest(TestCase):
+
+    def create_tag(self, title="Photography"):
+        return Tag.objects.create(title=title)
+
+    def test_tag(self):
+        t = self.create_tag()
+        self.assertTrue(isinstance(t, Tag))
+        self.assertEqual(t.__str__(), t.title)
